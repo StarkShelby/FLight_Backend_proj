8167bedb83f54bbe3966b8516ff5f34e1305973b
StarkShelby <your-email@example.com>
2026-07-23 17:52:45 +0530

    update models and migration

---
+++ src/migrations/20260723113245-update-city-airport-association.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const { QueryInterface } = require("sequelize");
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addConstraint("Airports", {
+      type: "FOREIGN KEY",
+      name: "city-foreign-key",
+      fields: ["citId"],
+      references: {
+        table: "Cities",
+        field: "id",
+      },
+      onUpdate: "CASCADE",
+      onDelete: "CASCADE",
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.removeConstraint("Airports", "city-foreign-key");
+  },
+};
