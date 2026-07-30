aa400e6fb3f2875760794a626ca99c7a6017be9e
StarkShelby <your-email@example.com>
2026-07-30 21:20:06 +0530

    added Seat tables with association with airplane

---
+++ src/utils/common/enums.js
@@ -0,0 +1,10 @@
+const SEAT_TYPE = {
+  ECONOMY: "Economy",
+  BUSINESS: "Business",
+  PREMIUM_ECONOMY: "Premium-economy",
+  FIRST_CLASS: "First-class",
+};
+
+module.exports = {
+  SEAT_TYPE,
+};
